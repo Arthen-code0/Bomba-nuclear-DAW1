0a2402e6081f274f7308b35a5a5970d824f86692
Daniel Riballo <driballoalvarez@safareyes.es>
2024-11-13 16:53:20 +0100

    coment

--- Lista_Barbacoa.py
+++ Lista_Barbacoa.py
@@ -3,7 +3,8 @@ lista_barbacoa = {
     "bebida":["cocacola", "fanta naranja","Nestea"],
     "cubata":["whisky_solo", "Ron"],
     "carbon":["encendedor","gas"],
-    "herramientas":["cuchillo"]
+    "herramientas":["cuchillo",["tenedores"],
     "Juegos":["Uno"],
-    "Tonto el que lo lea": ['bomba']
+    "Tonto el que lo lea": ['bomba'],
+    "tortilla":["cebolla"]
 }
