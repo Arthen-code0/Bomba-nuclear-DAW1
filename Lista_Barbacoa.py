1a752e07e8d381b39e83f58c8df4619a285782ef
Alejandro <acambrollemoraza@safareyes.es>
2024-11-13 16:19:41 +0100

    modificaciones

---
+++ Lista_Barbacoa.py
@@ -0,0 +1,9 @@
+lista_barbacoa = {
+    "comida":["salchicha", "chuletas","salchichón"],
+    "bebida":["cocacola", "fanta naranja","Nestea"],
+    "cubata":["whisky_solo", "Ron"],
+    "carbon":["encendedor","gas"],
+    "herramientas":["cuchillo"]
+    "Juegos":["Uno"],
+    "Tonto el que lo lea": ['bomba']
+}
