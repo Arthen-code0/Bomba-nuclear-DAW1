b017e9d4651976890090565727041198ea93b084
maria <marteagaramos@safareyes.es>
2024-11-13 19:59:39 +0100

    He eliminado una línea de código que estaba repetida y he añadido una nueva con la clave plastico

--- Lista_Barbacoa.py
+++ Lista_Barbacoa.py
@@ -5,13 +5,13 @@ lista_barbacoa = {
     "carbon":["encendedor","gas"],
     # No hace falta herramientas, se pueden comer con las manos
     "Juegos":["Uno", "parchis"],
-    "Tonto el que lo lea":["bomba"],
     "tortilla":["cebolla"],
-    "serranito":["tomate", "jamon", "pimiento", "tortilla francesa"]
+    "serranito":["tomate", "jamon", "pimiento", "tortilla francesa"],
     "Juegos":["Uno"],
     "Tonto el que lo lea": ['bomba'],
     "bocatas" : ["jamón", "chorizo"],
-    "Entretenimiento": ["PlayStation5", "balon"]
-    "Postre" : ["Tarta"]
+    "Entretenimiento": ["PlayStation5", "balon"],
+    "Postre" : ["Tarta"],
+    'plastico': ['vasos', 'cubiertos', 'platos']
 
 }
